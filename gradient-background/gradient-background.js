7cdebfaa8fac87599cadd906e4abdffa421ec3b9
Steffen70 <steffen@seventy.mx>
2021-04-06 21:33:47 +0200

    first commit

---
+++ gradient-background/gradient-background.js
@@ -0,0 +1,24 @@
+const root = document.documentElement;
+
+function setOffset(current) {
+    if (!current) return;
+
+    const rect = current.getBoundingClientRect();
+
+    var offset = {
+        top: rect.top + window.scrollY,
+        left: rect.left + window.scrollX,
+    };
+
+    if (current.classList.contains('navigation'))
+        root.style.setProperty('--scroll-offset', `${offset.left * -1}px`);
+    else {
+        current.style.setProperty('--offset-left', `${offset.left * -1}px`);
+        current.style.setProperty('--offset-top', `${offset.top * -1}px`);
+    }
+}
+
+const target = document.querySelectorAll('#svnt .grdt-bg, #svnt .navigation');
+window.addEventListener('resize', () => target.forEach(setOffset));
+
+window.onload = () => target.forEach(setOffset);
